b635890fe5db41f76716d3b8b09d386f25482ab3
Alonso Torres <alonso.torres@kaleidos.net>
2013-09-12 21:02:33 +0200

    java.sql.Timestamp JSONWriter

--- src/rest/tasks/resources.clj
+++ src/rest/tasks/resources.clj
@@ -2,7 +2,14 @@
   (:require 
     [rest.tasks.services :as services]
     [rest.tasks.models :as models]
-    [liberator.core :refer [defresource]]))
+    [liberator.core :refer [defresource]]
+    [clojure.data.json :refer[JSONWriter]] 
+    ))
+
+(defn- write-timestamp [x out]
+  (.print out (str \" x \")))
+
+(extend java.sql.Timestamp JSONWriter {:-write write-timestamp})
 
 (defresource tasks-resources
   :available-media-types ["application/json"]
