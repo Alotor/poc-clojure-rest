4053018338105bce043c0001a2d805b4976d7433
Alejandro Gómez <alejandro@dialelo.com>
2013-09-29 22:43:53 +0200

    Comment dependencies

--- project.clj
+++ project.clj
@@ -2,12 +2,19 @@
   :description "Proof of concept for REST/Backbone web stack"
   :dependencies
     [[org.clojure/clojure "1.5.1"]
-     [liberator "0.9.0"]
-     [compojure "1.1.5"]
+     ; Database
      [korma "0.3.0-RC5"]
      [lobos "1.0.0-beta1"]
      [postgresql "9.1-901.jdbc4"]
-     [hiccup "1.0.4"]]
+     ; HTTP routing
+     [ring/ring-core "1.2.0-beta1"]
+     [compojure "1.1.5"]
+     ; RESTful resources
+     [liberator "0.9.0"]
+     ; Templating
+     [hiccup "1.0.4"]
+     ; Authentication and authorization
+     [com.cemerick/friend "0.1.5"]]
   :plugins [[lein-ring "0.8.7"]]
   :ring {:handler rest.core/app}
   :eval-in-leiningen true)
