d3410a68e21da9710bfee67efe732f1e314f5b62
Andrey Antukh <niwi@niwi.be>
2013-11-04 23:34:36 +0100

    Add timestamped signatures.

--- test/rest/tests_auth.clj
+++ test/rest/tests_auth.clj
@@ -5,6 +5,20 @@
 (deftest auth-tests
   (testing "Signing/Unsigning with default keys"
     (let [signed (crypto/sign "foo")]
-      (is (= (crypto/sign "foo") signed))
-      (is (= (crypto/unsign signed) "foo")))))
-
+      (Thread/sleep 1000)
+      (is (not= (crypto/sign "foo") signed))
+      (is (= (crypto/unsign signed) "foo"))))
+  (testing "Signing/Unsigning with new key"
+    (let [default-key @crypto/*secret-key*]
+      (crypto/set-global-secret-key "foobar")
+      (let [signed (crypto/sign "foo")]
+        (Thread/sleep 1000)
+        (is (not= (crypto/sign "foo") signed))
+        (is (= (crypto/unsign signed) "foo")))
+      ;; Restore old key
+      (reset! crypto/*secret-key* default-key)))
+  (testing "Signing/Unsigning timestamped"
+    (let [signed (crypto/sign "foo")]
+      (Thread/sleep 1000)
+      ;; (is (= nil (crypto/unsign signed :max-age 2)))
+      (is (= "foo" (crypto/unsign signed :max-age 20))))))
